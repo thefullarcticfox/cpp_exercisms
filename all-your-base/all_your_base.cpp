2b4b68e0c54faaf4bbbb4a038cbdb8ab9c75eb85
Ivan <thefullarcticfox@users.noreply.github.com>
2021-03-29 23:08:00 +0300

    exercisms all your base check for overflows

--- all-your-base/all_your_base.cpp
+++ all-your-base/all_your_base.cpp
@@ -1,6 +1,7 @@
 #include "all_your_base.h"
 #include <stdexcept>
 #include <algorithm>
+#include <limits>
 
 namespace all_your_base {
 	using std::vector;
@@ -14,7 +15,11 @@ namespace all_your_base {
 		for (auto digit : indigits) {
 			if (digit >= inbase)
 				throw std::invalid_argument("impossible number for inbase");
+			if (num > std::numeric_limits<uint>::max() / inbase)
+				throw std::runtime_error("would overflow");
 			num *= inbase;
+			if (num > std::numeric_limits<uint>::max() - digit)
+				throw std::runtime_error("would overflow");
 			num += digit;
 		}
 
