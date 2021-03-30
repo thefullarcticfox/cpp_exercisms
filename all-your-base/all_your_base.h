0d6deccddd9918a23f7bc7a006df597b4df85010
Ivan <thefullarcticfox@users.noreply.github.com>
2021-03-30 03:50:10 +0300

    all-your-base comment delete

--- all-your-base/all_your_base.h
+++ all-your-base/all_your_base.h
@@ -3,7 +3,6 @@
 #include <vector>
 
 namespace all_your_base {
-	//	if indigits contain a value bigger than unsigned int it will throw exception
 	std::vector<unsigned int>	convert(unsigned int inbase,
 		const std::vector<unsigned int>& indigits, unsigned int outbase);
 }  // namespace all_your_base
