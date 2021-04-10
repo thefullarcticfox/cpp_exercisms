8708c85c6fd6e43366afb31e5437d12d49ccb3d0
Ivan <thefullarcticfox@users.noreply.github.com>
2021-04-10 17:29:34 +0300

    fix floats equality check

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -1,20 +1,25 @@
 #include <stdexcept>
-#include <cmath>
 #include <limits>
 #include "triangle.h"
 
 namespace triangle {
-	bool	doublecmp(double x, double y){
-		return fabs(x - y) <= std::numeric_limits<double>::epsilon();
+	bool	almost_equal(double x, double y,
+		double abs_epsilon = std::numeric_limits<double>::epsilon(),
+		double rel_epsilon = std::numeric_limits<double>::epsilon())
+	{
+		double diff = std::abs(x - y);
+		if (diff <= abs_epsilon)
+			return true;
+		return diff <= std::max(std::abs(x), std::abs(y)) * rel_epsilon;
 	}
 
 	flavor	kind(double x, double y, double z) {
 		if (x <= 0 || y <= 0 || z <= 0 ||
 			z > x + y || x > y + z || y > x + z)
 			throw std::domain_error("invalid triangle");
-		if (doublecmp(x, y) && doublecmp(y, z))
+		if (almost_equal(x, y) && almost_equal(y, z))
 			return flavor::equilateral;
-		if (doublecmp(x, y) || doublecmp(y, z) || doublecmp(x, z))
+		if (almost_equal(x, y) || almost_equal(y, z) || almost_equal(x, z))
 			return flavor::isosceles;
 		return flavor::scalene;
 	}
