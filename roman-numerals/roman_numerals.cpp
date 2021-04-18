6387e441dd1e4104c554fc236e20c1cf716f2b7c
Ivan <thefullarcticfox@users.noreply.github.com>
2021-04-18 19:25:39 +0300

    roman_numerals not done

--- roman-numerals/roman_numerals.cpp
+++ roman-numerals/roman_numerals.cpp
@@ -1,9 +1,28 @@
 #include "roman_numerals.h"
+#include <map>
 
 namespace roman_numerals {
+	using t_it = std::map<int, char>::const_iterator;
+
 	std::string	convert(int arabic_num) {
-		if (arabic_num == 1)
-			return ("I");
-		return ("");
+		const std::map< int, char, std::greater<int> >	roman_digits{
+			{1000, 'M'},
+			{500, 'D'},
+			{100, 'C'},
+			{50, 'L'},
+			{10, 'X'},
+			{5, 'V'},
+			{1, 'I'}
+		};
+
+		std::string	res;
+		for (auto& digit : roman_digits) {
+			while (arabic_num / digit.first) {
+				arabic_num -= digit.first;
+				res += digit.second;
+			}
+		}
+
+		return (res);
 	}
 }  // namespace roman_numerals
--- roman-numerals/roman_numerals_test.cpp
+++ roman-numerals/roman_numerals_test.cpp
@@ -6,7 +6,6 @@ TEST_CASE("one_yields_I")
     REQUIRE("I" == roman_numerals::convert(1));
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
 TEST_CASE("two_yields_II")
 {
     REQUIRE("II" == roman_numerals::convert(2));
@@ -91,4 +90,6 @@ TEST_CASE("three_thousand_yields_MMM")
 {
     REQUIRE("MMM" == roman_numerals::convert(3000));
 }
+
+#if defined(EXERCISM_RUN_ALL_TESTS)
 #endif
