69ef76335dab3ebf9a5531e622313aef7e40d415
Ivan <thefullarcticfox@users.noreply.github.com>
2021-04-20 19:38:23 +0300

    better armstrong

--- armstrong-numbers/armstrong_numbers.cpp
+++ armstrong-numbers/armstrong_numbers.cpp
@@ -3,23 +3,19 @@
 
 namespace armstrong_numbers {
 	bool	is_armstrong_number(const long n) {
-		long	tmp = n;
-		long	sum = 0;
 		std::vector<long>	digits;
 		digits.reserve(20);
 
 		//	push digits to vector
-		while (tmp != 0) {
+		for (long tmp = n; tmp != 0; tmp /= 10)
 			digits.push_back(tmp % 10);
-			tmp /= 10;
-		}
 
-		tmp = n;
 		//	find sum of digits powered by their count
-		for (size_t i = 0; i < digits.size(); i++) {
+		long	sum = 0;
+		for (int digit : digits) {
 			long	pow = 1;
-			for (size_t j = 0; j < digits.size(); j++)
-				pow *= digits.at(i);
+			for (size_t i = 0; i < digits.size(); i++)
+				pow *= digit;
 			sum += pow;
 		}
 
