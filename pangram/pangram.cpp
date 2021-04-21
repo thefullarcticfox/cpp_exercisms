fd0d744dab293e18e8f75a08f57126377ff60ee8
Ivan <thefullarcticfox@users.noreply.github.com>
2021-04-21 14:51:14 +0300

    pangram+

--- pangram/pangram.cpp
+++ pangram/pangram.cpp
@@ -1,12 +1,13 @@
 #include "pangram.h"
-#include <set>
+#include <unordered_set>
+#include <cctype>
 
 namespace pangram {
 	bool	is_pangram(const std::string& str) {
-		std::set<char>	letters;
+		std::unordered_set<char>	letters;
 		for (char letter : str)
 			if (std::isalpha(letter))
 				letters.insert(std::tolower(letter));
-		return (letters.size() == 26);
+		return letters.size() == 26;
 	}
 }  // namespace pangram
