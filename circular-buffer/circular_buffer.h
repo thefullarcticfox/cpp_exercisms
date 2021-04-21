7e3d84fb10656e018384ca4fcbc7d18a1f7edb92
Ivan <thefullarcticfox@users.noreply.github.com>
2021-04-21 17:37:15 +0300

    circular-buffer

---
+++ circular-buffer/circular_buffer.h
@@ -0,0 +1,45 @@
+#if !defined(CIRCULAR_BUFFER_H)
+#define CIRCULAR_BUFFER_H
+#include <stdexcept>
+#include <deque>
+
+namespace circular_buffer {
+	template <class T>
+	class circular_buffer {
+	private:
+		std::deque<T>	_buf;
+		size_t			_cap;
+	public:
+		explicit
+		circular_buffer(int capacity) : _cap(capacity) {}
+
+		T		read() {
+			if (_buf.empty())
+				throw std::domain_error("buffer is full");
+			T	data = _buf.front();
+			_buf.pop_front();
+			return data;
+		}
+
+		void	write(const T& data) {
+			if (_cap == _buf.size())
+				throw std::domain_error("buffer is full");
+			_buf.push_back(data);
+		}
+
+		void	overwrite(const T& data) {
+			if (_cap != _buf.size())
+				write(data);
+			else {
+				read();
+				write(data);
+			}
+		}
+
+		void	clear() {
+			_buf.clear();
+		}
+	};
+}  // namespace circular_buffer
+
+#endif // CIRCULAR_BUFFER_H
